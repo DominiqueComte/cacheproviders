205c3bb4c092dad00c16cf2a126e110b08c97ce4
Dominique Comte <dominique.comte.dev@gmail.com>
2022-06-27 22:53:56 +0200

    Added Redis to the cache providers

---
+++ src/main/java/ch/frankel/blog/cache/RedisProvider.java
@@ -0,0 +1,63 @@
+package ch.frankel.blog.cache;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.exceptions.JedisException;
+import redis.embedded.RedisServer;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class RedisProvider implements CacheProvider<Long, String> {
+
+	RedisServer server;
+	private JedisPool pool;
+
+	public RedisProvider() {
+		// bind address for the Redis server
+		final String serverIp = "127.0.0.1";
+		// find free port
+		final int port;
+		try {
+			final ServerSocket serverSocket = new ServerSocket(0);
+			port = serverSocket.getLocalPort();
+			serverSocket.close();
+		} catch (IOException e) {
+			throw new RuntimeException("Could not get a free port for the Redis server", e);
+		}
+
+		server = RedisServer.builder().setting("bind " + serverIp).port(port).build(); // bind to ignore Windows firewall popup each time the server starts
+		server.start();
+		pool = new JedisPool(serverIp, port);
+	}
+
+	@Override
+	public void put(Long key, String value) {
+		Jedis jedis = pool.getResource();
+		try {
+			jedis.set(String.valueOf(key), value);
+		} catch (JedisException e) {
+			// if something wrong happens, return it back to the pool
+			pool.returnBrokenResource(jedis);
+			jedis = null;
+		} finally {
+			// it's important to return the Jedis instance to the pool once you've finished using it
+			if (null != jedis) {
+				pool.returnResource(jedis);
+			}
+		}
+	}
+
+	@Override
+	public String get(Long key) {
+		Jedis jedis = pool.getResource();
+		return jedis.get(String.valueOf(key));
+	}
+
+	@Override
+	public void close() throws IOException {
+		pool.close();
+		server.stop();
+	}
+
+}
--- src/test/java/ch/frankel/blog/cache/CacheTests.java
+++ src/test/java/ch/frankel/blog/cache/CacheTests.java
@@ -4,10 +4,14 @@ import java.io.IOException;
 import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CacheTests {
+
+    private static final Logger log = LoggerFactory.getLogger(CacheTests.class);
 
     static Stream<CacheProvider<Long, String>> cacheProviders() {
         return Stream.of(
@@ -19,13 +23,15 @@ class CacheTests {
                 new CoherenceProvider(),
                 new IgniteProvider(),
                 new GeodeProvider(),
-                new HazelcastProvider()
+                new HazelcastProvider(),
+                new RedisProvider()
         );
     }
 
     @ParameterizedTest
     @MethodSource("cacheProviders")
     void whenPutShouldGet(CacheProvider<Long, String> provider) throws IOException {
+        log.info("Cache provider: {}", provider.getClass().getName());
         provider.put(1L, "One");
         var result = provider.get(1L);
         assertEquals("One", result);
